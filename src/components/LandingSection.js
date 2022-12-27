ad6909e3b8f10a65f8ba23e4eb7831cfdfca4f2b
Agnivesh Chaubey <agniveshvapi@gmail.com>
2022-12-28 00:11:07 +0530

    landing page completed

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Agnivesh!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -15,7 +15,13 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+  <VStack>
+    <Avatar src="https://media.licdn.com/dms/image/D4D03AQHM7nAhoNdEWA/profile-displayphoto-shrink_800_800/0/1672151466526?e=1677715200&v=beta&t=ZsH6Gej1e4mzHlhg29e-njPdjP0JtSkjJkKtwkXzOdc" />
+    <p>{greeting}</p>
+    <div></div>
+    <Heading>{bio1}</Heading>
+    <Heading>{bio2}</Heading>
+  </VStack>
   </FullScreenSection>
 );
 
